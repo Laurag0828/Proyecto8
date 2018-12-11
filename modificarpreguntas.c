0275d3256d4648eda126b6150e7cc0bcd1070788
JessiMassiel\Jessica <jessica.aviles8835@est.uca.edu.ni>
2018-12-11 15:03:22 -0600

    Modificar preguntas

--- modificarpreguntas.c
+++ modificarpreguntas.c
@@ -7,43 +7,71 @@
 
 //Función modificar preguntas
 int modificarPreguntas(char idCuestionario[10]){
-/*char idBuscar[20];
-int i; //Contador para encontrar la posicion de la pregunta en el archivo
-int encontrado; //variable de control para saber si se encuentra una pregunta
-FILE *f;
-struct Pregunta pregunta;
+
+    char idPregunta[20];
+    int i; //Contador para encontrar la posicion de la pregunta en el archivo
+    int encontrado; //variable de control para saber si se encuentra una pregunta
+    FILE *f;
+    struct Pregunta pregunta;
+    int respuesta=2;//Variable para la respuesta
 
 
-//Abre el archivo en modo lectura y escritura
-f = fopen("Preguntas.txt","r+");
-printf("-------------------------------------------------------\n");
-printf("Modificar una pregunta\n");
-printf("-------------------------------------------------------\n");
-//pedir datos del usuario a buscar
-printf("Ingrese el id de la pregunta a buscar-->");
-scanf("%s",idBuscar);
-
-
-
-//Leer el contenido del archivo y busca el usuario por el id
-encontrado=0; //Inicializa la variable de control en preguntas aun no encontrado
-i=0; //Inicializa el contador a cero que es el inicio del archivo
+    //Abre el archivo en modo lectura y escritura
+    f = fopen("Preguntas.txt","r+");
+    printf("-------------------------------------------------------\n");
+    printf("Modificar una pregunta\n");
+    printf("-------------------------------------------------------\n");
+    //pedir datos del usuario a buscar
+    printf("Ingrese el id de la pregunta a buscar-->");
+    scanf("%s",idPregunta);
 
 
 
-while(fread(&pregunta,sizeof(struct Pregunta),1,f)){//Lee el archivo con el tamaño de la  estructura Pregunta
-    if(strcmp(idBuscar,pregunta.idPregunta)==0){
+    //Leer el contenido del archivo y busca el usuario por el id
+    encontrado=0; //Inicializa la variable de control en preguntas aun no encontrado
+    i=0; //Inicializa el contador a cero que es el inicio del archivo
 
-       do{
+
+
+    while(fread(&pregunta,sizeof(struct Pregunta),1,f)){//Lee el archivo con el tamaño de la  estructura Pregunta
+        if(strcmp(idPregunta,pregunta.idPregunta)==0){
+           do{
                 printf("\n\nId: %s", pregunta.idPregunta);
-                printf("\nDescripción: %s", pregunta.texto);
-                printf("Desea modificar este cuestionario? (1=Si, 2=No)-->");
+                printf("\nDescripción: %s\n\n", pregunta.texto);
+                printf("Desea modificar esta pregunta? (1=Si, 2=No)-->");
                 scanf("%d",&respuesta);
                 fflush(stdin);
             } while(!(respuesta==1 || respuesta==2));//Repetir hasta que sea 1 o 2
 
+            if(respuesta==1){
+                printf("Ingrese el nuevo texto de la pregunta-->");
+                gets(pregunta.texto);
+                fflush(stdin);
+                printf("Ingrese cuantos puntos vale la pregunta-->");
+                scanf("%d",&pregunta.puntos);
+                fflush(stdin);
+                //Busca la posición del pregunta en el archivo
+                fseek(f,i*sizeof(pregunta),SEEK_SET);
+                //Escribe el pregunta modificado en esa posición
+                fwrite(&pregunta,sizeof(pregunta),1,f);
+                printf("Registro modificado!\n");
+            }
+            else{
+                printf("Acción cancelada!!!\n");
+            }
+            encontrado=1; //Si se encuentra una pregunta se cambia la variable de control a encontrado
+            break; //Finaliza la modificacion de la pregunta, termina el ciclo de busqueda
+        }
+        i++;//Si no lo encuentra aumenta la posicion a un registro mas
     }
-}*/
 
-return 0;
+    if(encontrado==0){//Si la variable de control esta en cero significa que no encontró el cuestionario en el archivo
+        printf("----La pregunta %s no está registrada -----\n",idPregunta);
+    }
+    //Cerrar el archivo
+    fclose(f);
+    system("pause");
+    system("cls");
+    return 0;
 }
+
